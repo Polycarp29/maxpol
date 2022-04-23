3acba58ee97c8df73b7f468178a0547cd6323bc8
P-Max <61562996+Polycarp29@users.noreply.github.com>
2022-04-23 10:01:01 -0400

    Add files via upload

---
+++ password generator.py
@@ -0,0 +1,11 @@
+import random
+lower = "abcdefghijklmnopqrstuvwxyz"
+upper = "ABCDEFGHIJKLMNOQRSTUVWXYZ"
+numbers = "1234567890"
+symbols ="/+_@#$%^&*()_+=[];'.><{}~`"
+all = lower+upper+numbers+symbols
+length = 25
+
+password ="".join(random.sample(all,length))
+point = (password)
+print(password)
